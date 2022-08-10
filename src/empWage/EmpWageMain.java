e6eec9942dc79e67b4952249b920a1e0284d7254
Veer1498 <singavarapuveerendra@gmail.com>
2022-08-10 22:53:51 +0530

    [Veer] Added : Ability to Compute Wages of Multiple Companies

--- src/empWage/EmpWageMain.java
+++ src/empWage/EmpWageMain.java
@@ -1,4 +1,6 @@
 package empWage;
+
+
 /**
  * Employee Wage Main function
  * @author Veer.singa
@@ -10,17 +12,20 @@ public class EmpWageMain {
 	/*
 	 * Created a Class EmpWageMain.
 	 * Created a function Main.
-	 * Created a New Object - EmpObject.
+	 * Created a New Object - empObject.
 	 * Call the Required Functions using new Object name.
-	 * Here, welcome and Method Functions were Called Directly.
+	 * Here, welcome and Method Functions were Called.
 	 */
 
+	
 	public static void main(String[] args) {
+		EmployeeWageMethod empObject = new EmployeeWageMethod();
+
+		empObject.welcome();
+		empObject.empWageMethod();	
 		
-		EmployeeWageMethod Empobject = new EmployeeWageMethod();
-		Empobject.Welcome();
-		Empobject.EmpWageMethod();
 	}
+	
 
 
 }
